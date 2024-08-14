e488d2da4c1732a185d195979a9cab6f9615c4fc
Gabriel Machado <112399650+gabrielmachado25@users.noreply.github.com>
2024-08-14 13:32:51 -0300

    Add files via upload

---
+++ exercicio008.js
@@ -0,0 +1,13 @@
+
+//Clicando 'Fn + F8', ele executa no terminal o que estou escrevendo
+
+var velocidade = 60.5 //carro andando a 60.5Km/h
+
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if (velocidade > 60){ //condição simples
+    /*if se lê como "se", então, se a velocidade for maior que 60 ele faz o
+    que está aqui dentro*/
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!!!`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ exercicio009.js
@@ -0,0 +1,10 @@
+
+var país = 'EUA'
+
+console.log(`Vivendo em ${país}`);
+
+if (país == 'Brasileiro') { //condição 
+    console.log('Você é Brasileiro!');
+} else { //se for diferente dessa primeira condição, faz isso:
+console.log('Você é Estrangeiro!');
+}
